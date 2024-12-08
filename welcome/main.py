7a88c6a49247b7c24ae1647345074c434e7036ac
wlsgur073 <wlsgur073@gmail.com>
2024-12-08 19:55:23 +0900

    오... Swagger UI 이쁘네요

--- welcome/main.py
+++ welcome/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", summary="테스트 api", tags=["테스트"])
 async def root():
+    '''
+    #### 오오오오오오~~~~~~.
+    - 마크다운도 제공해주다니 가슴이 웅장해집니다~~
+    '''
     return {"message": "Hello World"}
