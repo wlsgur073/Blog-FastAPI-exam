a51f1c59daec76f24362813e55235aeace2058b1
wlsgur073 <wlsgur073@gmail.com>
2024-12-23 21:11:34 +0900

    database 설정 동기에서 비동기로 변경

--- Blog/db/database.py
+++ Blog/db/database.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine, Connection
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.pool import QueuePool, NullPool
 from contextlib import contextmanager
@@ -8,39 +9,36 @@ from dotenv import load_dotenv
 import os
 
 # database connection URL
-# DATABASE_CONN = "mysql+mysqlconnector://root@localhost:3306/blog_db"
-load_dotenv() # default path -> .env file
-DATABASE_CONN = os.getenv("DATABASE_CONN")
-print("DATABASE_CONN: ", DATABASE_CONN)
+load_dotenv()
+ASYNC_DATABASE_CONN = os.getenv("ASYNC_DATABASE_CONN")
+print("ASYNC_DATABASE_CONN: ", ASYNC_DATABASE_CONN)
 
-engine = create_engine(DATABASE_CONN, #echo=True,
-                       poolclass=QueuePool,
-                       #poolclass=NullPool, # Connection Pool 사용하지 않음.
+engine = create_async_engine(ASYNC_DATABASE_CONN, #echo=True,
+                       # poolclass=QueuePool, # Pool class QueuePool cannot be used with asyncio engine
+                       # poolclass=NullPool, # Connection Pool 사용하지 않음.
                        pool_size=10, max_overflow=0,
                        pool_recycle=300)
 
-def direct_get_conn():
+async def direct_get_conn():
     conn = None
     try:
-        conn = engine.connect()
+        conn = await engine.connect()
         return conn
     except SQLAlchemyError as e:
         print("direct_get_conn Error: ", e)
-        # 대부분 Connection Pool이 넘어서 overflow 발생으로 에러가 발생함.
         raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, 
                             detail="The service you requested briefly encountered an internal problem.")
     
 # @contextmanager # Depends()가 기본적으로 사용함.
-def context_get_conn():
+async def context_get_conn():
     conn = None
     try:
-        conn = engine.connect()
+        conn = await engine.connect()
         yield conn
     except SQLAlchemyError as e:
         print("context_get_conn Error: ", e)
         raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, 
                             detail="The service you requested briefly encountered an internal problem.")
-                            
     finally:
         if conn:
-            conn.close()
+            await conn.close()
