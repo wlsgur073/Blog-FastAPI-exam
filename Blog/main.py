56152c2bfb693c0282a113abc0a2eda2bd240c08
wlsgur073 <wlsgur073@gmail.com>
2024-12-23 21:22:24 +0900

    FastAPI는 `lifespan` 개념을 이용하여 비동기적으로 처리하는 Connection pool를 비롯한 connection의 resource들을 깔끔하게 해제할  것을 권장한다.

--- Blog/main.py
+++ Blog/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from routes import blog
+from contextlib import asynccontextmanager
+from db.database import engine
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try:
+        # FastAPI instance 기동시 필요한 작업 수행
+        print("Starting up...")
+        yield # yield가 기준으로 before, after로 나뉨
+        
+        await engine.dispose()
+        
+        # FastAPI instance 종료시 필요한 작업 수행
+        print("Shutting down...")
+    finally:
+        pass
+
+app = FastAPI(lifespan=lifespan)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(blog.router)
