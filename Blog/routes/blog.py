e7fdfa2bbca980eb62e35c1b3d831d713985c9ad
wlsgur073 <wlsgur073@gmail.com>
2024-12-23 21:12:24 +0900

    동기 함수를 비동기 함수로 변경, 파일 업로드에 aiofiles 적용

--- Blog/routes/blog.py
+++ Blog/routes/blog.py
@@ -16,8 +16,7 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/")
 async def get_all_blogs(req: Request, conn: Connection = Depends(context_get_conn)):
-    
-    all_blogs = blog_svc.get_all_blogs(conn)
+    all_blogs = await blog_svc.get_all_blogs(conn)
     
     return templates.TemplateResponse(
         request = req,
@@ -27,8 +26,8 @@ async def get_all_blogs(req: Request, conn: Connection = Depends(context_get_con
    
 
 @router.get("/show/{id}")
-def get_blog_by_id(req: Request, id: int, conn: Connection = Depends(context_get_conn)):
-    blog = blog_svc.get_blog_by_id(conn, id)
+async def get_blog_by_id(req: Request, id: int, conn: Connection = Depends(context_get_conn)):
+    blog = await blog_svc.get_blog_by_id(conn, id)
     blog.content = util.newline_to_br(blog.content) # get_blog_by_id 함수가 수정에서도 재활용되기에 이렇게 변경.
     
     return templates.TemplateResponse(
@@ -38,7 +37,7 @@ def get_blog_by_id(req: Request, id: int, conn: Connection = Depends(context_get
     )
 
 @router.get("/new")
-def create_blog_ui(req: Request):
+async def create_blog_ui(req: Request):
     return templates.TemplateResponse(
         request = req,
         name = "new_blog.html",
@@ -46,28 +45,25 @@ def create_blog_ui(req: Request):
         )
     
 @router.post("/new")
-def create_blog(req: Request
+async def create_blog(req: Request
                 , title: str = Form(min_length=2, max_length=100)
                 , author: str = Form(max_length=100)
                 , content: str = Form(min_length=2, max_length=4000)
                 , imagefile: UploadFile | None = Form(None)
                 , conn: Connection = Depends(context_get_conn)):
     
-    print(f"imagefile: {imagefile}")
-    print(f"imagefile.filename: {imagefile.filename}")
-    
     image_loc = None
     if len(imagefile.filename.strip()) > 0: # filename의 길이가 0이면 이미지 파일이 없다는 것으로 체크
-        image_loc = blog_svc.upload_file(author=author, imagefile=imagefile)
-        blog_svc.create_blog(conn=conn, title=title, author=author, content=content, image_loc=image_loc)
+        image_loc = await blog_svc.upload_file(author=author, imagefile=imagefile)
+        await blog_svc.create_blog(conn=conn, title=title, author=author, content=content, image_loc=image_loc)
     else:
-        blog_svc.create_blog(conn=conn, title=title, author=author, content=content, image_loc=image_loc)
+        await blog_svc.create_blog(conn=conn, title=title, author=author, content=content, image_loc=image_loc)
     
     return RedirectResponse(url="/blogs", status_code=status.HTTP_302_FOUND)
     
 @router.get("/modify/{id}")
-def update_blog_ui(req: Request, id: int, conn = Depends(context_get_conn)):
-    blog = blog_svc.get_blog_by_id(conn, id = id)
+async def update_blog_ui(req: Request, id: int, conn = Depends(context_get_conn)):
+    blog = await blog_svc.get_blog_by_id(conn, id = id)
    
     return templates.TemplateResponse(
         request = req,
@@ -76,7 +72,7 @@ def update_blog_ui(req: Request, id: int, conn = Depends(context_get_conn)):
     )
    
 @router.post("/modify/{id}")
-def update_blog(req: Request, id: int
+async def update_blog(req: Request, id: int
                 , title: str = Form(min_length=2, max_length=100)
                 , author: str = Form(max_length=100)
                 , content: str = Form(min_length=2, max_length=4000)
@@ -85,21 +81,21 @@ def update_blog(req: Request, id: int
     
     image_loc = None
     if len(imagefile.filename.strip()) > 0: # filename의 길이가 0이면 이미지 파일이 없다는 것으로 체크
-        image_loc = blog_svc.upload_file(author=author, imagefile=imagefile)
-        blog_svc.update_blog(conn=conn, id=id, title=title, author=author, content=content, image_loc=image_loc)
+        image_loc = await blog_svc.upload_file(author=author, imagefile=imagefile)
+        await blog_svc.update_blog(conn=conn, id=id, title=title, author=author, content=content, image_loc=image_loc)
     else:
-        blog_svc.update_blog(conn=conn, id=id, title=title, author=author, content=content, image_loc=image_loc)
+        await blog_svc.update_blog(conn=conn, id=id, title=title, author=author, content=content, image_loc=image_loc)
    
     return RedirectResponse(url=f"/blogs/show/{id}", status_code=status.HTTP_302_FOUND)
     
 @router.post("/delete/{id}")
-def delete_blog(req: Request, id: int, conn: Connection = Depends(context_get_conn)):
+async def delete_blog(req: Request, id: int, conn: Connection = Depends(context_get_conn)):
     
-    blog = blog_svc.get_blog_by_id(conn, id = id) # blog.image_loc를 가져오기 위함
+    blog = await blog_svc.get_blog_by_id(conn, id = id) # blog.image_loc를 가져오기 위함
     # 만약 get_blog_by_id() 함수에서 업로드한 이미지가 없으면 defualt 이미지를 리턴하게 설정하고 삭제를 시도하면 default로 설정한 이미지가 지워진다.
     # 그런데 클라이언트 캐시에 캐시된 이미지가 있어서 일시적으로 삭제된 이미지가 출력된다 ㅋㅋ. 주의하자.
     
-    blog_svc.delete_blog(conn=conn, id=id, image_loc=blog.image_loc) 
+    await blog_svc.delete_blog(conn=conn, id=id, image_loc=blog.image_loc) 
     return JSONResponse(content={"message": "Blog deleted successfully."}) # 기본이 200이라 생략
     
     # 자바스크립트에서 fetch 이후 직접 redirect 하고 있기에 아래 코드 실행하면 에러 발생
--- Blog/services/blog_svc.py
+++ Blog/services/blog_svc.py
@@ -8,13 +8,14 @@ from typing import List
 from dotenv import load_dotenv
 import os
 import time
+import aiofiles as aio
 
 # Depends는 endpoint를 호출할 수 있는, router가 있는 곳에서 불러올 수 있기에, service에서는 매개변수로 받는다.
 
 load_dotenv()
 UPLOAD_DIR = os.getenv("UPLOAD_DIR")
 
-def get_all_blogs(conn: Connection) -> List: # return list type을 명시
+async def get_all_blogs(conn: Connection) -> List: # return list type을 명시
     try:
         query = """
             SELECT id, title, author, content,
@@ -27,7 +28,7 @@ def get_all_blogs(conn: Connection) -> List: # return list type을 명시
         # query = """
         #     SELECT id, title, author, content,image_loc, modified_dt FROM blog
         #         """
-        result = conn.execute(text(query))
+        result = await conn.execute(text(query))
         
         all_blogs = [BlogOutputData(id = row.id
                         , title = row.title
@@ -61,7 +62,7 @@ def get_all_blogs(conn: Connection) -> List: # return list type을 명시
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
                             , detail="An unknown service error occurred. Contact the administrator.")
 
-def get_blog_by_id(conn: Connection, id: int):
+async def get_blog_by_id(conn: Connection, id: int):
     try:
         query = """
             SELECT id, title, author, content, image_loc, modified_dt FROM blog
@@ -70,7 +71,7 @@ def get_blog_by_id(conn: Connection, id: int):
                 
         stmt = text(query)
         bind_stmt = stmt.bindparams(id=id)
-        result = conn.execute(bind_stmt)
+        result = await conn.execute(bind_stmt)
         
         # 만약에 결과가 없으면 에러 던지기
         if result.rowcount == 0:
@@ -96,7 +97,7 @@ def get_blog_by_id(conn: Connection, id: int):
                             , detail="The service you requested briefly encountered an internal problem.")
     
 # 이미 Form 체크를 해주고 파라미터로 들어오는 애들임
-def create_blog(conn: Connection, title: str, author: str
+async def create_blog(conn: Connection, title: str, author: str
                 , content: str, image_loc = None): # image_loc은 None이 들어오면 DB에 'None'으로 insert됨.
     try:
         query = f"""
@@ -104,8 +105,8 @@ def create_blog(conn: Connection, title: str, author: str
                 VALUES ('{title}', '{author}', '{content}', {util.none_to_null(image_loc, is_sqote=True)}, NOW())
                 """
         
-        conn.execute(text(query))
-        conn.commit()
+        await conn.execute(text(query))
+        await conn.commit()
         
     except SQLAlchemyError as e:
         print("create_blog Error: ", e)
@@ -114,7 +115,7 @@ def create_blog(conn: Connection, title: str, author: str
                             , detail="The request you made was not valid. Please check the input values.")
 
     
-def update_blog(conn: Connection, id: int, title: str, author: str, content: str, image_loc: str = None):
+async def update_blog(conn: Connection, id: int, title: str, author: str, content: str, image_loc: str = None):
     try:
         query = f"""
                 UPDATE blog
@@ -126,14 +127,14 @@ def update_blog(conn: Connection, id: int, title: str, author: str, content: str
         stmt = text(query)
         # python에서 bindvrariables를 사용할때는 None이 들어가면 Null로 번역해줌.
         bind_stmt = stmt.bindparams(id = id, image_loc = image_loc)
-        result = conn.execute(bind_stmt)
+        result = await conn.execute(bind_stmt)
         
         # app의 www url encoded로 오면 쿼리 파라미터에 값을 오입력할수도 있음. 그래서 예외처리함.
         if result.rowcount == 0:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                                 detail=f"Blog with id {id} not found.")
             
-        conn.commit()
+        await conn.commit()
         
     except SQLAlchemyError as e:
         print("update_blog Error: ", e)
@@ -141,7 +142,7 @@ def update_blog(conn: Connection, id: int, title: str, author: str, content: str
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST
                             , detail="The request you made was not valid. Please check the input values.")
 
-def upload_file(author: str, imagefile: UploadFile = None):
+async def upload_file(author: str, imagefile: UploadFile = None):
     try:
         user_dir = f"{UPLOAD_DIR}/{author}/"
         if not os.path.exists(user_dir):
@@ -151,9 +152,11 @@ def upload_file(author: str, imagefile: UploadFile = None):
         upload_filename = f"{filename_only}_{(int)(time.time())}{ext}"
         upload_image_loc = user_dir + upload_filename
         
-        with open(upload_image_loc, "wb") as outfile: # `wb` = `binary write`
-            while content := imagefile.file.read(1024):
-                outfile.write(content)
+        async with aio.open(upload_image_loc, "wb") as outfile: # `wb` = `binary write`
+            # while content := imagefile.file.read(1024):
+            while content := await imagefile.read(1024): # 원래 async로 선언돼 있는 함수임
+                await outfile.write(content)
+        # 읽는게 비동기인데 쓰는데 동기면 동기임
             
         print("Upload succeeded: ", upload_image_loc)
         
@@ -163,7 +166,7 @@ def upload_file(author: str, imagefile: UploadFile = None):
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
                             , detail="Image upload failed.")
         
-def delete_blog(conn: Connection, id: int, image_loc: str = None):
+async def delete_blog(conn: Connection, id: int, image_loc: str = None):
     try:
         # oltp에서는 대부분 bdinvariables를 사용한다.
         query = f"""
@@ -171,12 +174,12 @@ def delete_blog(conn: Connection, id: int, image_loc: str = None):
                 WHERE id = :id
                 """
         bind_stmt = text(query).bindparams(id = id)
-        result = conn.execute(bind_stmt)
+        result = await conn.execute(bind_stmt)
         
         if result.rowcount == 0:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found.")
         
-        conn.commit()
+        await conn.commit()
         
         if image_loc is not None:
             image_path = "." + image_loc # 경로가 '/static/~~'로 되어 있어서 상대
